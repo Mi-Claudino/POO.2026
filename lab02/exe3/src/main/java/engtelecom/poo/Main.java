375c8d5f0362095b453e051d78c775268022380d
Mi.Claudino <michel.p2004@aluno.ifsc.edu.br>
2026-03-11 09:42:00 -0300

    começando exercicio 3

---
+++ lab02/exe3/src/main/java/engtelecom/poo/Main.java
@@ -0,0 +1,11 @@
+package engtelecom.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String
+
+        }
+    }
+}
