6f113ca09e4f009d9449f637ec5f0ed3a1052ce2
Mi Claudino <michel.p2004@aluno.ifsc.edu.br>
2026-03-16 09:24:49 -0300

    Adicionando atividade de classes novas

---
+++ aula260316/contador/src/main/java/engtelecom/poo/Contador.java
@@ -0,0 +1,22 @@
+package engtelecom.poo;
+
+public class Contador {
+    private int numero;
+
+    public void aumentar(){
+        numero++;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setNumero(int n){
+        numero = n;
+    }
+}
+
+
+// incrementar em 1
+// Obter valor atual get
+// Atribuir um valor set
